404269353a6cd32f3497cad8067327ef2271fa96
kahtrus <kahtrus@gmail.com>
2015-03-30 17:22:17 +0300

    add widdget buttons clicklisteners and fix updates so cool so cool

--- app/src/main/java/co/hatrus/andrew/paint/widget/LoremViewsFactory.java
+++ app/src/main/java/co/hatrus/andrew/paint/widget/LoremViewsFactory.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Paint;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
@@ -17,7 +16,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 import co.hatrus.andrew.paint.BaseNoteActivity;
-import co.hatrus.andrew.paint.NoteLab;
 import co.hatrus.andrew.paint.NoteListActivity;
 import co.hatrus.andrew.paint.R;
 import co.hatrus.andrew.paint.model.CheckList;
@@ -29,6 +27,7 @@ import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class LoremViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+    private static final int DEFAULT_MAX_ITEMS_COUNT = 5;
     private static int ITEMS_COUNT = 5;
     private Realm mRealm;
     //private NoteLab mNoteLab;
@@ -49,23 +48,16 @@ public class LoremViewsFactory implements RemoteViewsService.RemoteViewsFactory 
 
     public LoremViewsFactory(Context ctxt, Intent intent) {
         this.ctxt=ctxt;
-//        appWidgetId=intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
-//                AppWidgetManager.INVALID_APPWIDGET_ID)- WidgetProvider.sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome;
-        appWidgetId = Integer.valueOf(intent.getData().getSchemeSpecificPart())
-                - WidgetProvider.sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome;
+        appWidgetId=intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+//        appWidgetId = Integer.valueOf(intent.getData().getSchemeSpecificPart())
+//                - WidgetProvider.sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome;
         Log.e("LoremViewsFactory", "constructor wid id: " + appWidgetId);
 
     }
     @Override
     public void onCreate() {
-// no-op
-        mRealm = Realm.getInstance(ctxt);
-        mRealmResults = mRealm.where(Note.class).findAllSorted("timeCreated",false);
-        long count = mRealm.where(Note.class).count();
-        if(count<ITEMS_COUNT)
-            ITEMS_COUNT = (int)count;
-        for(int i=0;i<ITEMS_COUNT;i++)
-            rowList.add(getRow(mRealmResults.get(i)));
+        onDataSetChanged();
     }
     @Override
     public void onDestroy() {
@@ -78,63 +70,6 @@ public class LoremViewsFactory implements RemoteViewsService.RemoteViewsFactory 
     @Override
     public RemoteViews getViewAt(int position) {
         return rowList.get(position);
-//        mRealm = Realm.getInstance(ctxt);
-//        mRealmResults = mRealm.where(Note.class).findAllSorted("timeCreated",false);
-//        Note note = mRealmResults.get(position);
-//        final RemoteViews row = new RemoteViews(ctxt.getPackageName(),
-//                R.layout.widget_text_row);
-//        row.removeAllViews(R.id.widget_checklist_container);
-//        row.setTextViewText(R.id.title_widget_row, note.getTitle());
-//
-//        switch (note.getType()) {
-//            case Note.NOTE_TYPE_TEXT:
-//                Log.e("WOWOWOWOWO","HRUHRU TEXT");
-//                TextNote textNote = mRealm.where(TextNote.class).equalTo("note.id", note.getId()).findFirst();
-//                row.setViewVisibility(R.id.text_widget_row, View.VISIBLE);
-//                row.setViewVisibility(R.id.widget_checklist_container, View.GONE);
-//                row.setViewVisibility(R.id.paint_widget_row, View.GONE);
-//                row.setTextViewText(R.id.text_widget_row, textNote.getText());
-//
-//                break;
-//            case Note.NOTE_TYPE_LIST:
-//                Log.e("WOWOWOWOWO","HRUHRU List");
-//                ListNote listNote = mRealm.where(ListNote.class).equalTo("note.id",note.getId()).findFirst();
-//                row.setViewVisibility(R.id.widget_checklist_container, View.VISIBLE);
-//                row.setViewVisibility(R.id.text_widget_row, View.GONE);
-//                row.setViewVisibility(R.id.paint_widget_row, View.GONE);
-//                RemoteViews rmChild;
-//                for(CheckList checkListItem:listNote.getNoteItems()) {
-//                    rmChild = new RemoteViews(ctxt.getPackageName(), R.layout.widget_checklist_item);
-//                    rmChild.setTextViewText(R.id.widget_checklist_item, checkListItem.getItem());
-//                    if(checkListItem.isChecked())
-//                        rmChild.setInt(R.id.widget_checklist_item,"setPaintFlags", Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
-//                    row.addView(R.id.widget_checklist_container, rmChild);
-//                }
-//                break;
-//            case Note.NOTE_TYPE_PAINT:
-//                PaintNote paintNote = mRealm.where(PaintNote.class).equalTo("note.id",note.getId()).findFirst();
-//                row.setViewVisibility(R.id.widget_checklist_container, View.GONE);
-//                row.setViewVisibility(R.id.text_widget_row, View.GONE);
-//                row.setViewVisibility(R.id.paint_widget_row, View.VISIBLE);
-//                File file = new File(ctxt.getExternalFilesDir("painta")+"/"+paintNote.getId()+".jpg");
-//                Log.e("WOWOWOWOWO","HRUHRU PAINT file path" + file.getAbsolutePath());
-//                if(!file.exists())
-//                    break;
-//                Log.e("WOWOWOWOWO","HRUHRU PAINT ");
-//                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-//                row.setViewVisibility(R.id.paint_widget_row, View.VISIBLE);
-//                row.setImageViewBitmap(R.id.paint_widget_row,bitmap);
-//                break;
-//            default:
-//                return null;
-//        }
-//        Intent i = new Intent(ctxt,BaseNoteActivity.class);
-//        i.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-//        i.putExtra(NoteListActivity.NOTE_ID_EXTRA, note.getId());
-//        i.putExtra(NoteListActivity.NOTE_TITLE_EXTRA, note.getTitle());
-//        i.putExtra(NoteListActivity.NOTE_TYPE_EXTRA, note.getType());
-//        row.setOnClickFillInIntent(R.id.row_container, i);
-//        return row;
     }
     @Override
     public RemoteViews getLoadingView() {
@@ -154,20 +89,27 @@ public class LoremViewsFactory implements RemoteViewsService.RemoteViewsFactory 
     }
     @Override
     public void onDataSetChanged() {
-        Log.e("LoremViewsFactory","onDataSC");
-
+        Log.d("LoremViewsFactory","onDataSetChanged IC "+ ITEMS_COUNT);
         mRealm = Realm.getInstance(ctxt);
         mRealmResults = mRealm.where(Note.class).findAllSorted("timeCreated",false);
         long count = mRealm.where(Note.class).count();
-        if(count<ITEMS_COUNT)
-            ITEMS_COUNT = (int)count;
-        else ITEMS_COUNT = 5;
+        if(count< DEFAULT_MAX_ITEMS_COUNT) {
+            ITEMS_COUNT = (int) count;
+            if (ITEMS_COUNT == 0) {
+                mRealm.close();
+                return;
+            }
+        }
+        else
+            ITEMS_COUNT= DEFAULT_MAX_ITEMS_COUNT;
         rowList.clear();
         for(int i=0;i<ITEMS_COUNT;i++)
             rowList.add(getRow(mRealmResults.get(i)));
 
+        mRealm.close();
 
     }
+    //returns widget list row depending on note type
     private RemoteViews getRow(Note note) {
         RemoteViews row = new RemoteViews(ctxt.getPackageName(),
                 R.layout.widget_text_row);
--- app/src/main/java/co/hatrus/andrew/paint/widget/WidgetProvider.java
+++ app/src/main/java/co/hatrus/andrew/paint/widget/WidgetProvider.java
@@ -13,40 +13,43 @@ import java.util.Random;
 import co.hatrus.andrew.paint.BaseNoteActivity;
 import co.hatrus.andrew.paint.NoteListActivity;
 import co.hatrus.andrew.paint.R;
+import co.hatrus.andrew.paint.model.Note;
 
 public class WidgetProvider extends AppWidgetProvider {
     public static String EXTRA_WORD=
             "com.commonsware.android.appwidget.lorem.WORD";
-    public static int sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome = 322;
 
     @Override
     public void onUpdate(Context ctxt, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
 
-        sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome = new Random().nextInt(1000);
-        Log.e("WidgetProvider", "randomnum: "+ sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome);
         for (int i=0; i<appWidgetIds.length; i++) {
             Log.e("WidgetProvider", "wid id: " + appWidgetIds[i]);
             appWidgetManager.updateAppWidget(appWidgetIds[i], null);
             Intent svcIntent=new Intent(ctxt, WidgetService.class);
-            svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]+sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome);
-            //svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
-            svcIntent.setData(Uri.fromParts("content", String.valueOf(appWidgetIds[i]+sRandomNumberForUpdatingWidgetIdTrickDontDoItAtHome), null));
+            svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            svcIntent.setData(Uri.fromParts("content", String.valueOf(appWidgetIds[i]), null));
             RemoteViews widget=new RemoteViews(ctxt.getPackageName(),
                     R.layout.widget_layout);
-
-            widget.setRemoteAdapter( appWidgetIds[i],R.id.widget_list,
+            widget.setRemoteAdapter(R.id.widget_list,
                     svcIntent);
-
-            Intent clickIntent=new Intent(ctxt, BaseNoteActivity.class);
-            PendingIntent clickPI=PendingIntent
-                    .getActivity(ctxt, 0,
-                            clickIntent,
-                            PendingIntent.FLAG_UPDATE_CURRENT);
-            widget.setPendingIntentTemplate(R.id.widget_list, clickPI);
+            // 3 PendingIntents and for buttons click to create new textnote/listnote/paintnote
+            widget.setOnClickPendingIntent(R.id.btn_new_textnote,getBaseNotePendingIntent(ctxt,Note.NOTE_TYPE_TEXT,"TextTag"));
+            widget.setOnClickPendingIntent(R.id.btn_new_paintnote,getBaseNotePendingIntent(ctxt,Note.NOTE_TYPE_PAINT,"PaintTag"));
+            widget.setOnClickPendingIntent(R.id.btn_new_listnote,getBaseNotePendingIntent(ctxt,Note.NOTE_TYPE_LIST,"ListTag"));
 
             appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds[i], R.id.widget_list);
         }
         super.onUpdate(ctxt, appWidgetManager, appWidgetIds);
+    }
+    private PendingIntent getBaseNotePendingIntent(Context ctxt, int noteType, String Tag) {
+        Intent clickIntent=new Intent(ctxt, BaseNoteActivity.class);
+        clickIntent.putExtra(NoteListActivity.NOTE_TYPE_EXTRA, noteType);
+        clickIntent.setAction(Tag);
+        return PendingIntent
+                .getActivity(ctxt, 0,
+                        clickIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
